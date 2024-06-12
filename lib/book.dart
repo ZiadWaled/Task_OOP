2c412712ce953f6d71fb2eafef8a40711f07f877
Ziad Waled <125661057+ZiadWaled@users.noreply.github.com>
2024-06-12 16:07:24 +0300

    Task 2

--- lib/book.dart
+++ lib/book.dart
@@ -1,5 +1,4 @@
-import 'dart:ffi';
-
+//Task 1
 class Book {
 
   String? title;
@@ -12,5 +11,17 @@ class Book {
     print('Title: $title');
     print('Author: $author');
     print('Number of Pages: $numOfPages');
+  }
+}
+//Task 2
+class Novel extends Book {
+  String? genre;
+
+ Novel(super.title, super.author, super.numberPages, this.genre);
+
+  @override
+  void displayInfo(){
+    super.displayInfo();
+    print('Genre: $genre');
   }
 }
